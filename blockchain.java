6c835c0a802dc2eb154a277218a878350bf74dbb
yein-lee <agop0310@gmail.com>
2021-06-06 06:27:34 +0900

    temp

--- blockchain.java
+++ blockchain.java
@@ -1,97 +1,99 @@
-import java.security.Security;
+import java.security.*;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
-public class blockchain {
-	public static ArrayList</*Block*/String> blockchain = new ArrayList</*Block*/String>();
-	public static HashMap<String,/*TransactionOutput*/String> UTXOs = new HashMap<String,/*TransactionOutput*/String>();
-	
-	public static int difficulty = 3;
-	public static float minimumTransaction = 0.1f;
+public class Transaction {
 
-	public static Transaction genesisTransaction;
-	
-	public static Boolean isChainValid() {
-		/*Block*/String currentBlock; 
-		/*Block*/String previousBlock;
-		String hashTarget = new String(new char[difficulty]).replace('\0', '0');
-		HashMap<String,T/*TransactionOutput*/String> tempUTXOs = new HashMap<String,/*TransactionOutput*/String>(); //a temporary working list of unspent transactions at a given block state.
-	
-		//tempUTXOs.put(genesisTransaction.outputs.get(0).id, genesisTransaction.outputs.get(0));
-		
-		//loop through blockchain to check hashes:
-		for(int i=1; i < blockchain.size(); i++) {
-			
-			currentBlock = blockchain.get(i);
-			previousBlock = blockchain.get(i-1);
-			//compare registered hash and calculated hash:
-			if(!currentBlock.hash.equals(currentBlock.calculateHash()) ){
-				System.out.println("#Current Hashes not equal");
-				return false;
-			}
-			//compare previous hash and registered previous hash
-			if(!previousBlock.hash.equals(currentBlock.previousHash) ) {
-				System.out.println("#Previous Hashes not equal");
-				return false;
-			}
-			//check if hash is solved
-			if(!currentBlock.hash.substring( 0, difficulty).equals(hashTarget)) {
-				System.out.println("#This block hasn't been mined");
-				return false;
-			}
-			
-			//loop thru blockchains transactions:
-			/*TransactionOutput*/String tempOutput;
-			for(int t=0; t <currentBlock.transactions.size(); t++) {
-				Transaction currentTransaction = currentBlock.transactions.get(t);
-				
-				if(!currentTransaction.verifySignature()) {
-					System.out.println("#Signature on Transaction(" + t + ") is Invalid");
-					return false; 
-				}
-				if(currentTransaction.getInputsValue() != currentTransaction.getOutputsValue()) {
-					System.out.println("#Inputs are note equal to outputs on Transaction(" + t + ")");
-					return false; 
-				}
-				
-				for(TransactionInput input: currentTransaction.inputs) {	
-					tempOutput = tempUTXOs.get(input.transactionOutputId);
-					
-					if(tempOutput == null) {
-						System.out.println("#Referenced input on Transaction(" + t + ") is Missing");
-						return false;
-					}
-					
-					if(input.UTXO.value != tempOutput.value) {
-						System.out.println("#Referenced input Transaction(" + t + ") value is Invalid");
-						return false;
-					}
-					
-					tempUTXOs.remove(input.transactionOutputId);
-				}
-				
-				for(TransactionOutput output: currentTransaction.outputs) {
-					tempUTXOs.put(output.id, output);
-				}
-				
-				if( currentTransaction.outputs.get(0).reciepient != currentTransaction.reciepient) {
-					System.out.println("#Transaction(" + t + ") output reciepient is not who it should be");
-					return false;
-				}
-				if( currentTransaction.outputs.get(1).reciepient != currentTransaction.sender) {
-					System.out.println("#Transaction(" + t + ") output 'change' is not sender.");
-					return false;
-				}
-				
-			}
-			
+	public String transactionId; //Contains a hash of transaction*
+	public PublicKey sender; //Senders address/public key.
+	public PublicKey reciepient; //Recipients address/public key.
+	public float value; //Contains the amount we wish to send to the recipient.
+	public byte[] signature; //This is to prevent anybody else from spending funds in our wallet.
+
+	public ArrayList<TransactionInput> inputs = new ArrayList<TransactionInput>();
+	public ArrayList<TransactionOutput> outputs = new ArrayList<TransactionOutput>();
+
+	private static int sequence = 0; //A rough count of how many transactions have been generated
+
+	// Constructor:
+	public Transaction(PublicKey from, PublicKey to, float value,  ArrayList<TransactionInput> inputs) {
+		this.sender = from;
+		this.reciepient = to;
+		this.value = value;
+		this.inputs = inputs;
+	}
+
+	public boolean processTransaction() {
+
+		if(verifySignature() == false) {
+			System.out.println("#Transaction Signature failed to verify");
+			return false;
 		}
-		System.out.println("Blockchain is valid");
+
+		//Gathers transaction inputs (Making sure they are unspent):
+		for(TransactionInput i : inputs) {
+			i.UTXO = NoobChain.UTXOs.get(i.transactionOutputId);
+		}
+
+		//Checks if transaction is valid:
+		if(getInputsValue() < NoobChain.minimumTransaction) {
+			System.out.println("Transaction Inputs too small: " + getInputsValue());
+			System.out.println("Please enter the amount greater than " + NoobChain.minimumTransaction);
+			return false;
+		}
+
+		//Generate transaction outputs:
+		float leftOver = getInputsValue() - value; //get value of inputs then the left over change:
+		transactionId = calulateHash();
+		outputs.add(new TransactionOutput( this.reciepient, value,transactionId)); //send value to recipient
+		outputs.add(new TransactionOutput( this.sender, leftOver,transactionId)); //send the left over 'change' back to sender
+
+		//Add outputs to Unspent list
+		for(TransactionOutput o : outputs) {
+			NoobChain.UTXOs.put(o.id , o);
+		}
+
+		//Remove transaction inputs from UTXO lists as spent:
+		for(TransactionInput i : inputs) {
+			if(i.UTXO == null) continue; //if Transaction can't be found skip it
+			NoobChain.UTXOs.remove(i.UTXO.id);
+		}
+
 		return true;
 	}
-	public static void addBlock(/*Block*/String newBlock) {
-		//newBlock.mineBlock(difficulty);
-		blockchain.add(newBlock);
+
+	public float getInputsValue() {
+		float total = 0;
+		for(TransactionInput i : inputs) {
+			if(i.UTXO == null) continue; //if Transaction can't be found skip it, This behavior may not be optimal.
+			total += i.UTXO.value;
+		}
+		return total;
+	}
+
+	public void generateSignature(PrivateKey privateKey) {
+		String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(reciepient) + Float.toString(value)	;
+		signature = StringUtil.applyECDSASig(privateKey,data);
+	}
+
+	public boolean verifySignature() {
+		String data = StringUtil.getStringFromKey(sender) + StringUtil.getStringFromKey(reciepient) + Float.toString(value)	;
+		return StringUtil.verifyECDSASig(sender, data, signature);
+	}
+
+	public float getOutputsValue() {
+		float total = 0;
+		for(TransactionOutput o : outputs) {
+			total += o.value;
+		}
+		return total;
+	}
+
+	private String calulateHash() {
+		sequence++; //increase the sequence to avoid 2 identical transactions having the same hash
+		return StringUtil.applySha256(
+				StringUtil.getStringFromKey(sender) +
+						StringUtil.getStringFromKey(reciepient) +
+						Float.toString(value) + sequence
+		);
 	}
 }
