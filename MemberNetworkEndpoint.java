358e9192ac16492f36b8a601841ac06c67600be5
haileykim1 <49053341+haileykim1@users.noreply.github.com>
2021-06-06 12:27:19 +0900

    Create membernote classes

---
+++ MemberNetworkEndpoint.java
@@ -0,0 +1,12 @@
+
+public class MemberNetworkEndpoint {
+	public String id;
+	public String ip;
+	public String port;
+	private String consortium;
+	private String networkBuffer;
+	public Boolean isConnected;
+	
+	
+	
+}
